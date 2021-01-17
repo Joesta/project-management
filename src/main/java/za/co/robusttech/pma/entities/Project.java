b72a9f263a49bfda73885286e5a0af36d8893588
Joesta <joestax@icloud.com>
2021-01-17 13:32:30 +0200

    add project controller and project template

--- src/main/java/za/co/robusttech/pma/entities/Project.java
+++ src/main/java/za/co/robusttech/pma/entities/Project.java
@@ -1,9 +1,11 @@
 package za.co.robusttech.pma.entities;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+@Entity
 public class Project {
 
 	@Id
