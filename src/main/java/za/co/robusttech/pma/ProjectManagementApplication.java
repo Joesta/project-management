fd29e467bc52f4724fd5de24ae8675e2abd3612f
Joesta <joestax@icloud.com>
2021-01-17 12:42:08 +0200

    renamed contextual package

--- src/main/java/za/co/robusttech/pma/ProjectManagementApplication.java
+++ src/main/java/za/co/robusttech/pma/ProjectManagementApplication.java
@@ -1,4 +1,4 @@
-package com.robusttech.pma;
+ package za.co.robusttech.pma;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
